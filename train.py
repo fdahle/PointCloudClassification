e6d41b703137df56575d2ddf057b2f41eadb7342
Felix Dahle <felix.dahle@googlemail.com>
2020-06-29 15:35:39 +0200

    Update train.py
    finalize classification

--- train.py
+++ train.py
@@ -2,16 +2,21 @@ import torch
 
 from model import *
 
+#train the model for classification
 def train(epochs, train_loader, val_loader=None, save=True):
 
+    #set device o cuda
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(device)
 
+    #init the NN
     pointnet = PointNet()
     pointnet.to(device)
 
+    #activate optimizer
     optimizer = torch.optim.Adam(pointnet.parameters(), lr=0.001)
 
+    #iterate through all epochs
     for epoch in range(epochs):
 
         pointnet.train()
@@ -32,6 +37,22 @@ def train(epochs, train_loader, val_loader=None, save=True):
                     print('[Epoch: %d, Batch: %4d / %4d], loss: %.3f' %
                         (epoch + 1, i + 1, len(train_loader), running_loss / 10))
                     running_loss = 0.0
+
+        pointnet.eval()
+        correct = total = 0
+
+        # validation
+        if val_loader:
+            with torch.no_grad():
+                for data in val_loader:
+                    inputs, labels = data['pointcloud'].to(device).float(), data['category'].to(device)
+                    outputs, __, __ = pointnet(inputs.transpose(1,2))
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+            val_acc = 100. * correct / total
+            print('Valid accuracy: %d %%' % val_acc)
+
 
 def pointnetloss(outputs, labels, m3x3, m64x64, alpha = 0.0001):
     criterion = torch.nn.NLLLoss()
